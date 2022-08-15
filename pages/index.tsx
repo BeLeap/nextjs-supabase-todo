e50e5c7bc1ef187eda35f0a4d39391395b130e55
BeLeap <beleap@beleap.codes>
2022-08-15 18:40:12 +0900

    refact:use component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,23 @@
-export default () => (
-  <>
-    Hello, World!
-  </>
-)
+import { Session } from "@supabase/supabase-js";
+import { useEffect, useState } from "react";
+import Auth from "../components/Auth";
+import Todo from "../components/Todo";
+import { supabase } from "../utils/supabase";
+
+export default () => {
+  const [session, setSession] = useState<Session | null>(null);
+
+  useEffect(() => {
+    setSession(supabase.auth.session());
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+  }, []);
+
+  return (
+    <>
+      { !session ? <Auth /> : <Todo /> }
+    </>
+  );
+}
